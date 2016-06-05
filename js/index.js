9034edd691d1df5fca544f7ad68c44c5241d72d6
sanpak <hillsfrontendweb@gmail.com>
2016-06-05 12:50:27 -0400

    wanna try a different approach. Manipulate the id directly

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
-$(document).ready(function() {
+//This is for .css().  It will add background color to the HTML.
+
+/*$(document).ready(function() {
 
   $('#stopButton').click(redLight);
   $('#slowButton').click(yellowLight);
@@ -25,5 +27,38 @@ $(document).ready(function() {
     $('#goLight').css('background-color','black');
   }
 
+
+});
+*/
+
+//want to try adding style to id.
+
+$(document).ready(function() {
+
+  $('#stopButton').click(redLight);
+  $('#slowButton').click(yellowLight);
+  $('#goButton').click(greenLight);
+
+  function redLight() {
+    clearLights();
+    $('#stopLight').css('background-color','red');
+  }
+
+  function yellowLight() {
+    clearLights();
+    $('#slowLight').css('background-color','yellow');
+  }
+
+  function greenLight() {
+    clearLights();
+    $('#goLight').css('background-color','green');
+  }
+
+  function clearLights() {
+    $('#stopLight').css('background-color','black');
+    $('#slowLight').css('background-color','black');
+    $('#goLight').css('background-color','black');
+  }
+  
 
 });
